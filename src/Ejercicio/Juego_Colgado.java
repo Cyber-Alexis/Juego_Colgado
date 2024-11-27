4c43c672cfa6d8e8794ae9e43d7f7191db29fd97
Cyber-Alexis <alexis.n.buza@alumnes.ilerna.com>
2024-11-27 02:02:07 +0100

    Parte 1
    Aqui lo que he hecho a sido utilizar el scanner para poder añadir caracteres, luego el System.out.println lo he usado para dar la bienvenida y preguntar cuantos jugadores quieren jugar, luego el int para que guarde una variable de un numero y luego he utilizado el while para hacer un bucle.

--- src/Ejercicio/Juego_Colgado.java
+++ src/Ejercicio/Juego_Colgado.java
@@ -1,10 +1,23 @@
 package Ejercicio;
 
+import java.util.Scanner;
+
 public class Juego_Colgado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner x = new Scanner(System.in); 
+	     
+		// Aqui solicitare el numero de jugadores y rondas	       
+		System.out.println("Bienvenidos al juego del Colgado");	        
+		System.out.print("¿Cuántos jugadores quieren jugar? (el mínimo es 2): ");	        
+		int numJugadores = x.nextInt();	       
+		while (numJugadores < 2) {	        	
+			System.out.print("Debe haber como minimo 2 jugadores. Intentalo otra vez: ");	           
+			numJugadores = x.nextInt();
+			
+		}
 	}
 
 }
